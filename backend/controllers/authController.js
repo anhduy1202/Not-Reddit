d468847bb770a50676df8446ba3db7cec0b99d7c
danieltruong <aduy1122@gmail.com>
2022-01-10 13:20:24 -0800

    Test deployment

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,7 +2,6 @@ const User = require("../models/User");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-
 const authController = {
   //REGISTER
   registerUser: async (req, res) => {
@@ -51,34 +50,33 @@ const authController = {
   loginUser: async (req, res) => {
     try {
       const user = await User.findOne({ username: req.body.username });
-      console.log("Running...");
-      if (!user) {
-        return res.status(404).json("Incorrect username");
-      }
-      const validPassword = await bcrypt.compare(
-        req.body.password,
-        user.password
-      );
-      if (!validPassword) {
-        return res.status(404).json("Incorrect password");
-      }
-      else if (user && validPassword) {
-        //Generate access token
-        const accessToken = authController.generateAccessToken(user);
-        //Generate refresh token
-        const refreshToken = authController.generateRefreshToken(user);
-        //STORE REFRESH TOKEN IN COOKIE
-        res.cookie("refreshToken", refreshToken, {
-          httpOnly: true,
-          secure:false,
-          path: "/",
-          sameSite: "none",
-        });
-        const { password, ...others } = user._doc;
-         res.status(200).json({ ...others, accessToken });
-      }
+      // console.log("Running...");
+      // if (!user) {
+      //   return res.status(404).json("Incorrect username");
+      // }
+      // const validPassword = await bcrypt.compare(
+      //   req.body.password,
+      //   user.password
+      // );
+      // if (!validPassword) {
+      //   return res.status(404).json("Incorrect password");
+      // }
+      // else if (user && validPassword) {
+      //   //Generate access token
+      //   const accessToken = authController.generateAccessToken(user);
+      //   //Generate refresh token
+      //   const refreshToken = authController.generateRefreshToken(user);
+      //   //STORE REFRESH TOKEN IN COOKIE
+      //   res.cookie("refreshToken", refreshToken, {
+      //     httpOnly: true,
+      //     secure:false,
+      //     path: "/",
+      //     sameSite: "none",
+      //   });
+      //   const { password, ...others } = user._doc;
+      res.status(200).json(user);
     } catch (err) {
-       res.status(500).json(err);
+      res.status(500).json(err);
     }
   },
 
@@ -97,7 +95,7 @@ const authController = {
       const newRefreshToken = authController.generateRefreshToken(user);
       res.cookie("refreshToken", refreshToken, {
         httpOnly: true,
-        secure:false,
+        secure: false,
         path: "/",
         sameSite: "strict",
       });
